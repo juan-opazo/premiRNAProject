8be43ada55a3f1a29e06a1a09b27bca3c91cbeb3
Juan Opazo <dopazo178@gmail.com>
2024-06-18 09:00:03 -0500

    add new features

--- FastaManager.py
+++ FastaManager.py
@@ -4,6 +4,7 @@ from Bio import SeqIO
 
 class FastaManager:
     FASTA = "fasta"
+    FA = "fa"
 
     @staticmethod
     def read_fasta(file_path):
@@ -19,7 +20,7 @@ class FastaManager:
         """Read FASTA files from input_dir and return sequences."""
         all_sequences = []
         for file_name in os.listdir(input_dir):
-            if file_name.endswith(f".{FastaManager.FASTA}"):
+            if file_name.endswith(f".{FastaManager.FASTA}") or file_name.endswith(f".{FastaManager.FA}"):
                 file_path = os.path.join(input_dir, file_name)
                 sequences = FastaManager.read_fasta(file_path)
                 all_sequences.extend(sequences)
@@ -29,3 +30,8 @@ class FastaManager:
     def create_fasta_dataset(input_dir, embedding):
         """Read FASTA files from input_dir and return tokenized sequences."""
         return embedding.tokenize_sequences(FastaManager.get_all_sequences(input_dir))
+
+    @staticmethod
+    def create_dataset(sequences, embedding):
+        """Read list of sequences and return tokenized sequences."""
+        return embedding.tokenize_sequences(sequences)
--- main.py
+++ main.py
@@ -3,40 +3,76 @@ from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.metrics import confusion_matrix, classification_report
+#from keras.preprocessing.text import Tokenizer
+#from keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
+#from keras.models import Sequential
+#from keras.layers import Embedding, LSTM, Dense
 
 from FastaManager import FastaManager
 from MiRNA2Vec import MiRNA2Vec
 from Visualizer import Visualizer
 
+"""
+def prepare_lstm_data(tokenized_sequences, max_len):
+    # Convert tokens to integer indices
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(tokenized_sequences)
+    sequences = tokenizer.texts_to_sequences(tokenized_sequences)
+    word_index = tokenizer.word_index
+
+    # Pad sequences
+    data = pad_sequences(sequences, maxlen=max_len, padding='post')
+
+    return data, word_index
+"""
+
 # Define the input directory
 input_directory = "./data/sequences"
 
 # Create miRNA2Vec instance
-miRNA2Vec = MiRNA2Vec(k_mers=3, vector_size=16, epochs=100)
+miRNA2Vec = MiRNA2Vec(k_mers=3, vector_size=16, epochs=5)
 
-# Create the dataset
-positive_tokenized_sequences = FastaManager.create_fasta_dataset(input_directory + "/positive_samples", miRNA2Vec)
-negative_tokenized_sequences = FastaManager.create_fasta_dataset(input_directory + "/negative_samples", miRNA2Vec)
-tokenized_sequences = positive_tokenized_sequences + negative_tokenized_sequences
+# Get sequences
+X_positive_sequences = FastaManager.get_all_sequences(input_directory + "/positive_samples")
+X_artificial_positive_sequences = FastaManager.get_all_sequences(input_directory + "/artificial_positive_samples")
+X_negative_sequences = FastaManager.get_all_sequences(input_directory + "/negative_samples")
+X = X_positive_sequences + X_artificial_positive_sequences + X_negative_sequences
 
-# Train the Word2Vec model
-miRNA2Vec.train_word2vec(tokenized_sequences)
+print(f"positive sequences: {len(X_positive_sequences)}")
+print(f"artificial positive sequences: {len(X_artificial_positive_sequences)}")
+print(f"negative sequences: {len(X_negative_sequences)}")
 
-# Get embeddings for sequences
-X_positive = miRNA2Vec.get_average_embeddings(FastaManager.get_all_sequences(input_directory + "/positive_samples"))
-X_negative = miRNA2Vec.get_average_embeddings(FastaManager.get_all_sequences(input_directory + "/negative_samples"))
-X = X_positive + X_negative
 
 # Create target list
-y_positive = np.ones(len(X_positive))
-y_negative = np.zeros(len(X_negative))
-y = np.append(y_positive, y_negative)
+y_positive = np.ones(len(X_positive_sequences))
+y_positive_with_data_augmentation = np.ones(len(X_artificial_positive_sequences))
+y_negative = np.zeros(len(X_negative_sequences))
+y = np.append(y_positive, y_positive_with_data_augmentation)
+y = np.append(y, y_negative)
+
 
 # Split data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
+
+# Create the dataset for Word2Vec
+tokenized_sequences = FastaManager.create_dataset(X_train, miRNA2Vec)
+""" intentar con RNA2Vec y DNA2Vec"""
+"""dna2vec model puede traer errores por versiones distintas"""
+print(tokenized_sequences[0:2])
+quit()
+# Train the Word2Vec model
+miRNA2Vec.train_word2vec(tokenized_sequences)
+# miRNA2Vec.load_model('./pretrained/dna2vec.w2v', tokenized_sequences)
+
+# Get embeddings for sequences
+X_train = miRNA2Vec.get_average_embeddings(X_train)
+X_test = miRNA2Vec.get_average_embeddings(X_test)
+print(f"X_train for SVM model: {X_train[0:2]}")
+
 # Train SVM
-svm_model = SVC(kernel='rbf')
+svm_model = SVC(kernel='rbf', class_weight="balanced")
 svm_model.fit(X_train, y_train)
 
 # Predict and evaluate
@@ -61,15 +97,22 @@ new_embedding = miRNA2Vec.get_average_embeddings([new_sequence])
 new_prediction = svm_model.predict(new_embedding)
 print(f"Prediction for new sequence: {new_prediction[0]}")
 
-Visualizer.show_2d_scatter(X, y, title="PCA visualization of sequence embeddings")
+Visualizer.show_variance(X_test)
 
-Visualizer.show_3d_scatter(X, y, title="PCA visualization of sequence embeddings")
+Visualizer.show_2d_scatter(X_test, y_test, title="PCA visualization of sequence embeddings")
+
+Visualizer.show_3d_scatter(X_test, y_test, title="PCA visualization of sequence embeddings")
 
 # Save the model
-# output_model_path = "./data/processed/word2vec_model.model"
-# save_embeddings(w2v_model, output_model_path)
+output_model_path = "./pretrained/miRNAFromWord2Vec.w2v"
+miRNA2Vec.model.wv.save(output_model_path)
 
 # Example: Getting embeddings for k-mers
 # kmer = "AUG"
 # embedding = miRNA2Vec.model.wv[kmer]
 # print(f"Embedding for {kmer} with {np.size(embedding)} elements: {embedding}")
+
+
+"""comparar FS4"""
+"""comparar rapidez con screening de secuencia larga"""
+"""armar secuencia aleatoriamente con secuencias del dataset, <SEQ1><nucleotidos_aleatorios><SEQ2><nt_aleatorios>..."""
